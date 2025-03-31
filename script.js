96effab8cfa4ddca0a04fc7de7ad2ca865ccab16
Raj Kumar <168063593+Rajkumargit45@users.noreply.github.com>
2025-04-01 00:15:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const clock= document.getElementById('clock')
+let date= new Date()
+
+setInterval(function(){
+    // console.log(date.toLocaleTimeString());
+    clock.innerHTML= date.toLocaleTimeString()
+},1000);
